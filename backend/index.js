482b3cbd0e479917afc8dfea2149b34c69749ee7
siddhantmendhe <38434303+siddhantmendhe@users.noreply.github.com>
2022-12-10 14:55:59 +0530

    login and signup test

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,13 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.get('/api/v1/login', (req, res) => {
+    res.send('Hello login!')
+  })
+
+  app.get('/api/v1/signup', (req, res) => {
+    res.send('Hello Signup!')
+  })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
